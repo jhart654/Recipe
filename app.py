f24d258cd5be298175246fa09b89f093d0efc560
jhart654 <143129762+jhart654@users.noreply.github.com>
2024-05-27 20:03:44 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
+import dropbox
+
+
+app = Flask(__name__)
+
+conn = psycopg2.connect("dbname=gloliwoe user=gloliwoe password=DzE9p8MVZgwbohsY4iXuO-R7tCmBr-TI host=fanny.db.elephantsql.com port=5432")
+cur = conn.cursor()   
+
+DROPBOX_ACCESS_TOKEN = 'sl.B2D54HuOOyGPK23WcQC79aP6ZN56UbWdgzrayNy2CpaY4w-hrH1uHtWy1cRcThsOlWKm6nFVgF7reH9Cgpn6KyrKREnGKRhG07et6QirEHohz8Nfl_qQRffLju5e3y2fNTgcwyaJA2Dbv_Y'
+dbx = dropbox.Dropbox(DROPBOX_ACCESS_TOKEN)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    recipe_name = request.form['recipe_name']
+    ingredients = request.form['ingredients']
+    instructions = request.form['instructions']
+    cooking_time = request.form['cooking_time']
+    difficulty_level = request.form['difficulty_level']
+    meal_type = request.form['meal_type']
+    cuisine_type = request.form['cuisine_type']
+    photo = request.files['photo']
+
+    response = dbx.files_upload(photo.read(), f'/photos/{photo.filename}')
+
+    shared_link = dbx.sharing_create_shared_link(response.path_display).url
+    cur.execute('INSERT INTO recipes (recipe_name, ingredients, instructions, cooking_time, difficulty_level, meal_type, cuisine_type, photo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (recipe_name, ingredients, instructions, cooking_time, difficulty_level, meal_type, cuisine_type, shared_link))
+    conn.commit()
+
+    return redirect(url_for('success'))
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/recipe')
+def recipe():
+    cur = conn.cursor()
+    cur.execute('SELECT id, recipe_name, ingredients, instructions, cooking_time, difficulty_level, meal_type, cuisine_type FROM recipes')
+    items = cur.fetchall()
+    cur.close()
+    return render_template('display.html', items=items)
+
+@app.route('/details/<int:item_id>')
+def details(item_id):
+    cur.execute('SELECT * FROM recipes where id = %s', (item_id,))
+    item = cur.fetchone()
+    return render_template('details.html', item=item)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
